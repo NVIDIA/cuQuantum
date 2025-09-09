f6984ed15c9a672a9524f24774b4a595a9fb2ef6
Benedikt Kloss <bkloss@nvidia.com>
2025-09-08 20:41:42 -0700

    Add licence header to file missing it.

--- python/cuquantum/densitymat/_internal/typemaps.py
+++ python/cuquantum/densitymat/_internal/typemaps.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 import cuquantum.bindings.cudensitymat as cudm
 
 CUDENSITYMAT_COMPUTE_TYPE_MAP = {
